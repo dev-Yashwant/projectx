54ccd0c2efd5102176a36454f808330d7305eefc
StormBreaker2022 <114773085+StormBreaker2022@users.noreply.github.com>
2024-04-12 12:10:15 +0530

    login page create

--- website/project_x_frontend/src/components/Login.jsx
+++ website/project_x_frontend/src/components/Login.jsx
@@ -144,7 +144,7 @@ const LoginForm = () => {
         <p>
           Created with <i className="fa fa-heart"></i> by
           <a target="_blank" href="https://florin-pop.com">Florin Pop</a>
-          - Read how I created this and how you can join the challenge
+          - Read more
           <a target="_blank" href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/">here</a>.
         </p>
       </footer>
